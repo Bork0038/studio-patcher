93821e831a5cb2f9b0536a5be1159b50dff2bdee
Chris <71100852+Bork0038@users.noreply.github.com>
2023-03-22 16:00:35 -0400

    fixed internal studio
    swapped to sig from vsk

--- src-tauri/src/patches/internal_studio.rs
+++ src-tauri/src/patches/internal_studio.rs
@@ -16,7 +16,7 @@ impl InternalStudioPatch {
         let scanner = Scanner::new( &data );
 
         let addr = scanner.scan(
-            &IDAPat::new( "48 89 5C 24 08 57 48 83 EC 50 0F 10 01 48 8D 4C 24 30 49 8B D8 48 8B FA 0F 29 44 24 30 E8 ?? ?? ?? ?? 84 C0 0F 85 C5 00 00 00 0F 10 03 48 C7 44 24 28 08 00 00 00" )
+            &IDAPat::new( "83 CB 04 89 5E 64" )
         ).map_or(
             Err( "Failed to find ::hasInternalPermission"),
             | addr | Ok(addr)
@@ -25,12 +25,9 @@ impl InternalStudioPatch {
         patch_segment(
             data, 
             vec![ 
-                0xE9, 0xE0, 0x00, 0x00, 0x00, 
-                0x90, 
-                0x90, 
-                0x90 
+                0x83, 0xCB, 0x05, 
             ],
-            addr + 10
+            addr
         );
 
         Ok(())
