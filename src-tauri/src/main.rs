67c2f79d8e7a6a54a766633a973c1f8e035eff68
chris-hardy2 <71100852+Bork0038@users.noreply.github.com>
2023-03-27 11:40:26 -0400

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,31 +1,32 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// mod commands;
-// mod patches;
-mod binary;
-// mod scanner;
+mod commands;
+mod patches;
+// mod binary;
+mod scanner;
 
-use binary::{ Binary, Section, SectionHeader };
+// use binary::{ Binary, Section, SectionHeader };
 
 #[tokio::main]
 async fn main() {
-    unsafe {
-        let mut binary = Binary::new(
-            include_bytes!( "../../chrome.exe" )
-        );
+    // unsafe {
+    //     let mut binary = Binary::new(
+    //         include_bytes!( "../../chrome.exe" )
+    //     );
 
-        let new_section = Section::new(
-            ".test",
-            Vec::with_capacity( 0x1000 )
-        );
-        binary.add_section( new_section );
+    //     let new_section = Section::new(
+    //         ".test",
+    //         Vec::with_capacity( 0x1000 )
+    //     );
+    //     binary.add_section( new_section );
 
-        let out = binary.compile();
-    }
-    // tauri::Builder::default()
-    //     .invoke_handler(tauri::generate_handler![
-    //         commands::install_patches
-    //     ])
-    //     .run(tauri::generate_context!())
-    //     .expect("error while running tauri application");
+    //     let out = binary.compile();
+    // }
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            commands::install_patches
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
