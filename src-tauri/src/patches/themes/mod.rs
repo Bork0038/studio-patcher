6dcf62fa2ab20983b952b5c55db61bc18c8e5db8
Chris <71100852+Bork0038@users.noreply.github.com>
2023-04-19 13:41:47 -0400

    test

--- src-tauri/src/patches/themes/mod.rs
+++ src-tauri/src/patches/themes/mod.rs
@@ -18,7 +18,7 @@ lazy_static! {
         
     ];
 
-    static ref THEME_LOAD_PAT: &'static str = "41 8D 57 2B 48 8D 0D 91 40 05 03 FF 15 ?? ?? ?? ?? 48 89 85 00 01 00 00 41 8D 57 2A 48 8D 0D A9 40 05 03 FF 15 ?? ?? ?? ?? 48 89 85 08 01 00 00 41 8D 57 02 48 8D 8D 70 01 00 00 E8 ?? ?? ?? ?? 4C 8B F0 48 89 85 70 01 00 00 4C 8D 78 10 4C 89 BD 80 01 00 00 48 8B F8 48 8D 9D 00 01 00 00 0F 1F 44 00 00 48 8B D3 48 8B CF FF 15 ?? ?? ?? ?? 48 83 C7 08 48 83 C3 08 48 8D 85 10 01 00 00 48 3B D8 75 E0 48 89 BD A0 01 00 00 48 89 BD 78 01 00 00 4C 8B 0D FB 13 52 02 BA 08 00 00 00 44 8D 42 FA 48 8D 8D 00 01 00 00 E8 ?? ?? ?? ??";
+    static ref THEME_LOAD_PAT: &'static str = "41 8D 57 2B 48 8D 0D ?? ?? ?? ?? FF 15 ?? ?? ?? ?? 48 89 85 00 01 00 00 41 8D 57 2A 48 8D 0D ?? ?? ?? ?? FF 15 ?? ?? ?? ?? 48 89 85 08 01 00 00 41 8D 57 02 48 8D 8D 70 01 00 00 E8 ?? ?? ?? ?? 4C 8B F0 48 89 85 70 01 00 00 4C 8D 78 10 4C 89 BD 80 01 00 00 48 8B F8 48 8D 9D 00 01 00 00 0F 1F 44 00 00 48 8B D3 48 8B CF FF 15 ?? ?? ?? ?? 48 83 C7 08 48 83 C3 08 48 8D 85 10 01 00 00 48 3B D8 75 E0 48 89 BD A0 01 00 00 48 89 BD 78 01 00 00 4C 8B 0D ?? ?? ?? ?? BA 08 00 00 00 44 8D 42 FA 48 8D 8D 00 01 00 00 E8 ?? ?? ?? ?? 49 8B C6 48 89 45 60 4C 3B F7 0F 84 10 0F 00 00 48 BB B3 01 00 00 00 01 00 00 0F 1F 40 00 0F 1F 84 00 00 00 00 00 48 8B D0";
     static ref THEME_LOAD_LEN: usize = THEME_LOAD_PAT.split_whitespace().count();
 }
 
@@ -157,41 +157,29 @@ impl ThemesPatch {
             
         }?;
 
-        let static_start_register =  {
-            let inst = instructions
-                .get( 12 )
-                .map_or(
-                    Err("Failed to find instruction"),
-                    | inst | Ok( inst )
-                )?;
-
-            if inst.op0_kind() != OpKind::Memory {
-                Err("Instruction op0 not memory" )
-            } else {
-                Ok( inst ) 
-            }
-        }?;
-
         let offset = themes_rva - ( text_rva + theme_load_addr as u32 );
         let mut encoder = Encoder::new( 64 );
         let mut out_inst = Vec::new();
 
-        // mov rax, [rip]
-        // add rax, ????
         {
+            // lea rax, [rip]
             let mut inst = Instruction::new();
-            let data = [ 0x48, 0x8B, 0x05, 0x00, 0x00, 0x00, 0x00 ];
+            inst.set_code( Code::Lea_r64_m );
+            inst.set_op0_kind( OpKind::Register );
+            inst.set_op0_register( Register::RAX );
+            inst.set_op1_kind( OpKind::Memory );
+            inst.set_memory_base( Register::RIP );
+            inst.set_memory_displacement32( 7 );
 
-            let mut decoder = Decoder::with_ip( 64, &data, text_rva as u64 + theme_load_addr as u64, DecoderOptions::NONE );
-            decoder.decode_out( &mut inst );
             out_inst.push( inst );
-
+            
+            // add rax, ????
             let mut inst = Instruction::new();
             inst.set_code( Code::Add_rm64_imm32 );
             inst.set_op0_kind( OpKind::Register );
             inst.set_op0_register( Register::RAX );
             inst.set_op1_kind( OpKind::Immediate32to64 );
-            inst.set_immediate32to64( offset as i64 - data.len() as i64 );
+            inst.set_immediate32to64( offset as i64 + section.len() as i64 * 8 );
 
             out_inst.push( inst );
         }
@@ -203,23 +191,6 @@ impl ThemesPatch {
             inst.set_code( Code::Mov_rm64_r64 );
             inst.set_op0_kind( OpKind::Register );
             inst.set_op0_register( start_register );
-            inst.set_op1_kind( OpKind::Register );
-            inst.set_op1_register( Register::RAX );
-
-            out_inst.push( inst );
-        }
-
-        // mov [static], rax
-        {
-            let mut inst = Instruction::new();
-
-            inst.set_code( Code::Mov_rm64_r64 );
-            inst.set_op0_kind( OpKind::Memory );
-            inst.set_memory_base( static_start_register.memory_base() );
-            inst.set_memory_displ_size( static_start_register.memory_displ_size() );
-            inst.set_memory_displacement64( static_start_register.memory_displacement64() );
-            inst.set_memory_index( static_start_register.memory_index() );
-            inst.set_memory_index_scale( static_start_register.memory_index_scale() );
             inst.set_op1_kind( OpKind::Register );
             inst.set_op1_register( Register::RAX );
 
@@ -252,40 +223,138 @@ impl ThemesPatch {
             out_inst.push( inst );
         }
 
+        let insts_clone = [ 
+            12, // mov [static], rax
+            13, // lea reg, [rax+??]
+            14, // mov [static], reg
+            26, // mov [static], start
+            27, // mov [static], start
+            33, // mov rax, register
+            34, // mov [static], rax
+            35, // cmp reg, reg
+            36, // je addy
+            37, // mov reg, ????
+            38, // nop dword ptr [reg]
+            39, // nop dword ptr [reg+reg]
+            // 40, // mov reg, reg
+        ];
+
+        for i in insts_clone {
+            let inst = instructions
+                .get( i )
+                .map_or(
+                    Err("Failed to find instruction"),
+                    | inst | Ok( inst )
+                )?;
+
+            out_inst.push( *inst );
+        }
+
+
+        {
+            let instruction = instructions
+                .get( 40 )
+                .map_or(
+                    Err("Failed to find instruction"),
+                    | inst | Ok( inst )
+                )?;
+
+
+            // mov reg, [rax]
+            let mut inst = Instruction::new();
+    
+            inst.set_code( Code::Mov_r64_rm64 );
+            inst.set_op0_kind( OpKind::Register );
+            inst.set_op0_register( instruction.op0_register() );
+            inst.set_op1_kind( OpKind::Memory );
+            inst.set_memory_index( Register::None );
+            inst.set_memory_base( instruction.op1_register() );
+
+            out_inst.push( inst );
+                
+            // add rax, [rip]
+            let mut inst = Instruction::new();
+
+            inst.set_code( Code::Add_r64_rm64 );
+            inst.set_op0_kind( OpKind::Register );
+            inst.set_op0_register( Register::RDX );
+            inst.set_op1_kind( OpKind::Memory );
+            inst.set_memory_index( Register::None );
+            inst.set_memory_base( Register::RIP );
+            inst.set_memory_displacement64( 7 );
+
+            out_inst.push( inst );
+        }
+
         let mut encoder = Encoder::new( 64 );
         for inst in out_inst {
-            encoder.encode( &inst, theme_load_addr as u64)?;
+            encoder.encode( &inst, 0 )?;
         }
         
-        let mut inst_buf = encoder.take_buffer();
-        let mut data_buf = Vec::from_iter( std::iter::repeat( 0x90 ).take( THEME_LOAD_LEN.clone() - inst_buf.len() ) );
-        inst_buf.append( &mut data_buf );
-        
-        for i in 0..inst_buf.len() {
-            text_data[ theme_load_addr + i ] = inst_buf[ i ];
+        let mut inst_data = encoder.take_buffer();
+        let inst_size = inst_data.len();
+        let mut data_buf = Vec::from_iter( std::iter::repeat( 0x90 ).take( THEME_LOAD_LEN.clone() - inst_size ) );
+        inst_data.append( &mut data_buf );
+
+        for i in 0..inst_data.len() {
+            text_data[ theme_load_addr + i ] = inst_data[ i ];
         }
+
+        let rip = text_rva + (theme_load_addr + inst_size) as u32;
+        for offset in offset_map {
+            let offset = (themes_rva + offset as u32) - rip;
+
+            section.append( &mut offset.to_le_bytes().to_vec() );
+        }
+        section.append( &mut Vec::from([ 0xFF; 8 ]));
+     
+        bin.set_section_data( ".themes", section )?;
+        bin.set_section_data( ".text", text_data )?;
+
+        
+        // let mut formatter = FastFormatter::new();
+        // for (index, instruction) in instructions.iter().enumerate() {
+        //     let mut string = String::new();
+        //     formatter.format( &instruction, &mut string );
+
+        //     println!("[{}] {}", index, string)
+        // }
+        // // mov [static], rax
+        // {
+        //     let mut inst = Instruction::new();
+
+        //     inst.set_code( Code::Mov_rm64_r64 );
+        //     inst.set_op0_kind( OpKind::Memory );
+        //     inst.set_memory_base( static_start_register.memory_base() );
+        //     inst.set_memory_displ_size( static_start_register.memory_displ_size() );
+        //     inst.set_memory_displacement64( static_start_register.memory_displacement64() );
+        //     inst.set_memory_index( static_start_register.memory_index() );
+        //     inst.set_memory_index_scale( static_start_register.memory_index_scale() );
+        //     inst.set_op1_kind( OpKind::Register );
+        //     inst.set_op1_register( Register::RAX );
+
+        //     out_inst.push( inst );
+        // }
 
 
         // shift the theme load loop back by ?? bytes to add space for more instructions
-        const PADDING_BYTES: usize = 20;
+        // const PADDING_BYTES: usize = 20;
 
-        let loop_start = theme_load_addr + THEME_LOAD_LEN.clone();
-        let mut loop_end = loop_start;
-        while &text_data[ loop_end..loop_end + 4 ] != &[ 0x48, 0x8B, 0xD0, 0x48 ] {
-            loop_end += 1;
-        }
+        // let loop_start = theme_load_addr + THEME_LOAD_LEN.clone();
+        // let mut loop_end = loop_start;
+        // while &text_data[ loop_end..loop_end + 4 ] != &[ 0x48, 0x8B, 0xD0, 0x48 ] {
+        //     loop_end += 1;
+        // }
 
-        let loop_size = loop_end - loop_start + 3;
-        for i in 0..loop_size {
-            let i = loop_size - i - 1;
+        // let loop_size = loop_end - loop_start + 3;
+        // for i in 0..loop_size {
+        //     let i = loop_size - i - 1;
 
-            text_data[ loop_start - PADDING_BYTES - loop_size - 1 + i ] = text_data[ loop_start + i ];
-            text_data[ loop_start + i ] = 0x90;
-        }
+        //     text_data[ loop_start - PADDING_BYTES - loop_size - 1 + i ] = text_data[ loop_start + i ];
+        //     text_data[ loop_start + i ] = 0x90;
+        // }
         
-        let new_offset = text_data[ loop_start + loop_size - PADDING_BYTES ];
-
-        bin.set_section_data( ".text", text_data )?;
+        // let new_offset = text_data[ loop_start + loop_size - PADDING_BYTES ];
         // let offset = themes_rva - ( text_rva + theme_load_addr as u32 );
         // let theme_array_offset = offset + 3 + section.len() as u32;
 
